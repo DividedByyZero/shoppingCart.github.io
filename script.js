4509b6660f87584a6baa0fc9ec3a73a340dd7302
Nabeel Ahsan <104347823+DividedByyZero@users.noreply.github.com>
2023-09-14 02:04:44 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let list = document.getElementById('category')
+list.addEventListener("change",showProduct);
+function showProduct(e){
+    let search = list.options[list.selectedIndex].textContent;
+    fetch(`https://fakestoreapi.com/products/category/${search.toLowerCase()}`)
+            .then(res=>res.json())
+            .then(json=>showTable(json))
+    console.log(list.options[list.selectedIndex].textContent);
+}
+function showTable(data){
+    let table = document.getElementById('table');
+    table.innerHTML="";
+    data.forEach((element)=> {
+        let productName = element.title;
+        let img = element.image;
+        let price = element.price;
+        let desc = element.description;
+        let id = element.id;
+        let ht =`<tr>
+        <th><img src="${img}" width="250" height="160"></img></th>
+        <th>
+        <h3>${productName}</h3>
+        <span>CODE : L-${id}</span>
+        <h6>${desc}</h6>
+        <h4><bold>${price} $ </bold></h4>
+        <th><button id="addToCart">Add To Cart </button></th>
+        </tr>`
+        let table = document.getElementById('table');
+        table.innerHTML += ht;
+        console.log(element)
+    });
+}
